8121a1bd91780234e925376964ebaf65d1e7fac3
Shahrom <shahrom2011@list.ru>
2024-02-20 21:43:23 +0500

    213123

---
+++ s16.02.24.js
@@ -0,0 +1,57 @@
+var a = "9";
+var b = "13";
+
+// "9" + "13" = "913"
+
+// "9" - "13" = "-4"
+
+// a = a + b; // 913
+// b = a - b; // 900
+// a = a - b; // 13
+
+console.log(`a = ${a}, b = ${b}`)
+
+
+var a = 99;
+var b = 100;
+
+const pravda = a > b ?
+                    a === b ?
+                            "1 часть" : "2 часть" :
+                     a !== b ?
+                            "3 часть" : "4 часть";
+                       //true;
+                       //pravda = 1 
+                       //если 
+                       //false
+                       //pravda = 0
+
+                       console.log(pravda);
+
+//if(pravda) {
+//    console.log("НЕТ это Не правильно") ("ДА это Правильно")
+//}
+//
+//else { 
+//   console.log("НЕТ это Не правильно")
+//}
+
+
+// switch (pravda) {
+
+//     case "1 часть"
+//        console.log("1 часть");
+
+//     case "2 часть"
+//        console.log("2 часть");
+    
+//     break;
+
+//     case  "3 часть"
+//        console.log("3 часть");
+
+//     break;
+
+//     case "4 часть"
+//       console.log("4 часть")
+// }
