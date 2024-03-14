4b72fbaf884a5dde513f1548a2f3a7c0466f0d84
Shahrom <shahrom2011@list.ru>
2024-03-14 21:40:55 +0500

    28042011

--- 11.3.24.js
+++ 11.3.24.js
@@ -1,13 +1,12 @@
-const findMax = (numbers) => {
-    let maxLength = 0;
+const findMax = (str) => {
+    let max = 0;
     
     for (let i = 0; i < str.length; i++) {
-        if (str[i].length > max) {
+        if (str[i] > max) {
             max = str[i];
         }
     }
-    
-    return longestWord;
+    return max;
 }
   
  console.log(findMax([1, 0, -5, 40, 10, -100, 48])); 
--- 13.3.24.js
+++ 13.3.24.js
@@ -1,4 +1,4 @@
 var shifu = [1,2,3,4,5,6,7,8,9,10]
 
-//console.log(shifu.filter(number => number > 10))
-console.log(shifu.map(number => number * 5))
+console.log(shifu.filter(number => number > 10)) //.filter
+console.log(shifu.map(number => number + 5)) //.map
